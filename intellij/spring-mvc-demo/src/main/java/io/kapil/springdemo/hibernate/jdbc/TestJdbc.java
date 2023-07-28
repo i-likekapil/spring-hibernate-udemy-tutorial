61d74dfa7586b96274b8a36f5b80bc429e26eebb
Kapil Kaushik <kapil.kaushik@cavisson.com>
2023-07-28 17:49:23 +0530

    19. Setting Up Hibernate Development Environment

---
+++ intellij/spring-mvc-demo/src/main/java/io/kapil/springdemo/hibernate/jdbc/TestJdbc.java
@@ -0,0 +1,30 @@
+package io.kapil.springdemo.hibernate.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * @author Kapil Kaushik
+ * @project spring-mvc-demo
+ * @Date 28/07/23
+ * @Time 5:43 pm
+ */
+
+public class TestJdbc {
+    public static void main(String[] args) {
+
+
+        try {
+            String JDBC_URL = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
+            String USER_NAME = "cavisson";
+            String PASSWORD = "Kapil@1234";
+
+            Connection con = DriverManager.getConnection(JDBC_URL, USER_NAME, PASSWORD);
+            System.out.println("connection successful " + con);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
